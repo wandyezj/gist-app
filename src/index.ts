8b432c3ff67bf3dfec44ab98ca405a2fd38ecef0
wandyezj <wandyezj@gmail.com>
2023-11-08 06:55:50 -0800

    save client_secret in local storage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
 import { website, clock } from "./website";
 clock();
 console.log(website());
+
+const localStorageKeyClientSecret = "client_secret";
+
+function buttonSaveClientSecret() {
+    const clientSecret = (document.getElementById("input_client_secret") as HTMLInputElement).value;
+    window.localStorage.setItem(localStorageKeyClientSecret, clientSecret);
+}
+
+function getClientSecret(): string | undefined {
+    return window.localStorage.getItem(localStorageKeyClientSecret) || undefined;
+}
+
+function initialize() {
+    // allow saving of client secret
+    (document.getElementById("button_save_client_secret") as HTMLButtonElement).addEventListener(
+        "click",
+        buttonSaveClientSecret
+    );
+
+    // display previously saved secret
+    const clientSecret = getClientSecret();
+    if (clientSecret) {
+        (document.getElementById("input_client_secret") as HTMLInputElement).value = clientSecret;
+    }
+}
+
+// TODO: implement calls to get auth token and use it to get data from the API
+
+initialize();
