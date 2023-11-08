2d1ff5640b999ad8fc4db4e8bf0a9a4331f57d04
wandyezj <wandyezj@gmail.com>
2023-11-08 07:21:11 -0800

    save client id and start redirect

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,58 @@ import { website, clock } from "./website";
 clock();
 console.log(website());
 
+// HTML element ids
+const inputClientId = "input_client_id";
+const inputClientSecret = "input_client_secret";
+const buttonSaveClient = "button_save_client";
+const buttonAuth = "button_auth";
+
+const localStorageKeyClientId = "client_id";
 const localStorageKeyClientSecret = "client_secret";
 
-function buttonSaveClientSecret() {
-    const clientSecret = (document.getElementById("input_client_secret") as HTMLInputElement).value;
+function clickButtonSaveClient() {
+    const clientId = (document.getElementById(inputClientId) as HTMLInputElement).value;
+    window.localStorage.setItem(localStorageKeyClientId, clientId);
+
+    const clientSecret = (document.getElementById(inputClientSecret) as HTMLInputElement).value;
     window.localStorage.setItem(localStorageKeyClientSecret, clientSecret);
+}
+
+// Retrieve from the input fields instead? That way they are visible?
+function getClientId(): string | undefined {
+    return window.localStorage.getItem(localStorageKeyClientId) || undefined;
 }
 
 function getClientSecret(): string | undefined {
     return window.localStorage.getItem(localStorageKeyClientSecret) || undefined;
 }
 
-function initialize() {
-    // allow saving of client secret
-    (document.getElementById("button_save_client_secret") as HTMLButtonElement).addEventListener(
-        "click",
-        buttonSaveClientSecret
-    );
+// TODO: implement calls to get auth token and use it to get data from the API
 
-    // display previously saved secret
-    const clientSecret = getClientSecret();
-    if (clientSecret) {
-        (document.getElementById("input_client_secret") as HTMLInputElement).value = clientSecret;
-    }
+function clickButtonAuth() {
+    const clientId = getClientId();
+
+    // TODO: specify redirect URL (these need to be registered in the application)
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
 }
 
-// TODO: implement calls to get auth token and use it to get data from the API
+function initialize() {
+    // TODO: check for code in URL
+
+    // display previously saved values
+    const clientId = getClientSecret();
+    if (clientId) {
+        (document.getElementById(inputClientId) as HTMLInputElement).value = clientId;
+    }
+
+    const clientSecret = getClientSecret();
+    if (clientSecret) {
+        (document.getElementById(inputClientSecret) as HTMLInputElement).value = clientSecret;
+    }
+
+    // allow saving of client secret
+    (document.getElementById(buttonSaveClient) as HTMLButtonElement).addEventListener("click", clickButtonSaveClient);
+    (document.getElementById(buttonAuth) as HTMLButtonElement).addEventListener("click", clickButtonAuth);
+}
 
 initialize();
